b18251990cb7738ee481b5eac7cd94567ffe7e18
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-02 20:45:21 +0200

    Create functions
    20-45

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,41 @@
+import React, {Component} from "react";
 import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions }  from "./FeedbackOptions/FeedbackOptions.jsx";
+import { render } from "react-dom";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+}
+updateCount = state => {
+  this.setState(prevState => ({ [state]: prevState[state] + 1 }));
+}
+
+countTotalFeedback = () =>
+  Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+countPositiveFeedbackPercentage = () => {
+  return this.countTotalFeedback()
+    ? ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0)
+    : '0';
 };
+
+render() {
+  const { good, neutral, bad } = this.state;
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options = {this.state}
+          onLeaveFeedback = {this.updateCount}/>
+      </Section>
+
+      {
+
+      }
+    </>
+  )
+}
